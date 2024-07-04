512eeb27f087738ecb951c09882cf9c5a325a214
Daiki Shibata <121758973+h0tcl0g@users.noreply.github.com>
2024-07-04 16:22:39 +0900

    Add files via upload

--- Python/myserial.py
+++ Python/myserial.py
@@ -5,7 +5,7 @@ import time
 ### Swift->Python シリアル通信 設定 ###
 # 受信側アドレス設定
 # 受信側IP
-SrcIP = "192.168.121.3"
+SrcIP = "192.168.36.31"
 # 受信側ポート番号
 SrcPort = 12345
 # 受信側アドレスをtuppleに格納
@@ -21,13 +21,9 @@ udpServSock.bind(SrcAddr)
 ########################
 
 ### Python->Arduino シリアル通信　設定 ###
-def toggle_led():
-    ser.write(b'1') # LEDを点灯
-    time.sleep(5) # 5秒点灯状態で待機
-    ser.write(b'0')    # LEDを消灯
 
 # シリアルポートの設定
-ser = serial.Serial('/dev/cu.usbmodem111401', 9600)
+ser = serial.Serial('/dev/cu.usbmodem11401', 9600)
 
 time.sleep(2)   # 接続待機
 
@@ -40,12 +36,32 @@ while True:
     data, addr = udpServSock.recvfrom(BUFSIZE)
     # 受信データと送信アドレスを出力
     print(data.decode(), addr)
-    if data.decode() == "exe":
-        toggle_led()
+    if data.decode() == "reset":
+        ser.write(b'reset')
 
     if data.decode() == "quit":
         break
 
+    if data.decode() == "1":
+        ser.write(b'1')
+    elif data.decode() == "2":
+        ser.write(b'2')
+    elif data.decode() == "3":
+        ser.write(b'3')
+    elif data.decode() == "4":
+        ser.write(b'4')
+    elif data.decode() == "5":
+        ser.write(b'5')
+    elif data.decode() == "6":
+        ser.write(b'6')
+    elif data.decode() == "7":
+        ser.write(b'7')
+    elif data.decode() == "8":
+        ser.write(b'8')
+    elif data.decode() == "9":
+        ser.write(b'9')
+    elif data.decode() == "10":
+        ser.write(b'10')
 
 # シリアルポートを閉じる
 ser.close()
